a804be038d21a49ef42f842ec3d34e77d3bf2eac
kongra <kongra@gmail.com>
2018-10-10 13:41:10 +0200

    Dodany program Euler1a.

---
+++ 2018-10-10/Euler1a.java
@@ -0,0 +1,18 @@
+class Euler1a {
+
+  public static void main(String[] args) {
+    // 1. 0, 1, 2, 3, 4, 5, 6, 7, ..., 999
+    int i;
+    i = 0;
+    while(i < 1000) {
+      System.out.println("Wartość i wynosi " + i);
+      i = i + 1;
+    }
+
+    // 2. Oznacz liczby podzielne przez 3 lub 5
+    //    0, 3, 5, 6, 9, 12, 15, ..., 999
+    // 3. Posumuj oznaczone liczby
+    // 4. Wyświetl wynik sumowania
+  }
+
+}
--- 2018-10-10/Program.java
+++ 2018-10-10/Program.java
@@ -5,6 +5,7 @@ class Program {
   }
 
   public static void main(String[] args) {
+    System.out.println("Hello World");
     System.out.println(3.14159);
     System.out.println(f(5));
   }
