4edb208088bee61e46bbdf560e3cc58c8adecc5d
kongra <kongra@gmail.com>
2018-10-10 12:16:36 +0200

    Zaczynamy zajęcia.

---
+++ 2018-10-10/Euler1.java
@@ -0,0 +1,12 @@
+class Euler1 {
+
+  public static void main(String[] args) {
+    int i;
+    i = 0;
+
+    while(i < 1000) {
+      System.out.println("Aktualnie i jest " + i);
+      i = i + 1;
+    }
+  }
+}
