6df0ba79d196c0a2f3a7251305ec63f03b208a77
kongra <kongra@gmail.com>
2018-10-17 11:31:48 +0200

    Update.'

--- Stationary/2018-10-10/Euler1.java
+++ Stationary/2018-10-10/Euler1.java
@@ -1,12 +1,24 @@
 class Euler1 {
 
+  static boolean divisibleBy3(int n) {
+    return n % 3 == 0;
+  }
+
+  static boolean divisibleBy5(int n) {
+    return n % 5 == 0;
+  }
+
   public static void main(String[] args) {
-    int i;
-    i = 0;
+    int i = 0;
+    int sum = 0;
 
     while(i < 1000) {
-      System.out.println("Aktualnie i jest " + i);
-      i = i + 1;
+      if (divisibleBy3(i) || divisibleBy5(i)) {
+        sum += i;
+      }
+      i += 1;
     }
+
+    System.out.println("sum is " + sum);
   }
 }
