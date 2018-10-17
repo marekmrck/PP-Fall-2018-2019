c8c52850cb3af1f331aaec7b9d629047ceb1ca1d
kongra <kongra@gmail.com>
2018-10-17 13:40:42 +0200

    Update.

--- Stationary/2018-10-10/Euler1a.java
+++ Stationary/2018-10-10/Euler1a.java
@@ -1,18 +1,24 @@
 class Euler1a {
 
+  static boolean divisibleBy3(int n) {
+    return n % 3 == 0;
+  }
+
+  static boolean divisibleBy5(int n) {
+    return n % 5 == 0;
+  }
+
   public static void main(String[] args) {
-    // 1. 0, 1, 2, 3, 4, 5, 6, 7, ..., 999
-    int i;
-    i = 0;
+    int i = 0;
+    int sum = 0;
     while(i < 1000) {
-      System.out.println("Wartość i wynosi " + i);
-      i = i + 1;
+      if (divisibleBy3(i) || divisibleBy5(i)) {
+        sum += i;
+      }
+      i += 1;
     }
 
-    // 2. Oznacz liczby podzielne przez 3 lub 5
-    //    0, 3, 5, 6, 9, 12, 15, ..., 999
-    // 3. Posumuj oznaczone liczby
-    // 4. Wyświetl wynik sumowania
+    System.out.println(sum);
   }
 
 }
